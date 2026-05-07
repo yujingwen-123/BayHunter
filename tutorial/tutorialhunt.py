89064fc583f0c56360042d3bda2f668a5893aec0
BreadCatee_v_ <163295161+yujingwen-123@users.noreply.github.com>
2026-05-07 19:42:20 +0800

    Remove RF baseline offset using pre-arrival mean correction

--- tutorial/tutorialhunt.py
+++ tutorial/tutorialhunt.py
@@ -7,6 +7,8 @@
 # #############################
 
 import os
+import argparse
+import glob
 # set os.environment variables to ensure that numerical computations
 # do not do multiprocessing !! Essential !! Do not change !
 os.environ["MKL_NUM_THREADS"] = "1"
@@ -34,12 +36,23 @@ logging.basicConfig(format=formatter, level=logging.INFO)
 logger = logging.getLogger()
 
 
+parser = argparse.ArgumentParser(
+    description='BayHunter tutorial inversion launcher')
+parser.add_argument(
+    '--inv', dest='inv_mode', default='joint',
+    choices=['joint', 'rf', 'swd'],
+    help='Inversion target mode: joint (RF+SWD), rf only, or swd only.')
+args = parser.parse_args()
+
+
 #
 # ------------------------------------------------------------  obs SYNTH DATA
 #
 # Load priors and initparams from config.ini or simply create dictionaries.
 initfile = 'config.ini'
 priors, initparams = utils.load_params(initfile)
+if not initparams['station'].endswith('_%s' % args.inv_mode):
+    initparams['station'] = '%s_%s' % (initparams['station'], args.inv_mode)
 
 # Load observed data (synthetic test data)
 xsw, _ysw = np.loadtxt('observed/st3_rdispph.dat').T
@@ -48,11 +61,13 @@ xrf, _yrf = np.loadtxt('observed/st3_prf.dat').T
 # add noise to create observed data
 # order of noise values (correlation, amplitude):
 # noise = [corr1, sigma1, corr2, sigma2] for 2 targets
-noise = [0.0, 0.012, 0.98, 0.005]
-ysw_err = SynthObs.compute_expnoise(_ysw, corr=noise[0], sigma=noise[1])
+noise_swd = [0.0, 0.012]
+noise_rf = [0.98, 0.005]
+ysw_err = SynthObs.compute_expnoise(_ysw, corr=noise_swd[0], sigma=noise_swd[1])
 ysw = _ysw + ysw_err
-yrf_err = SynthObs.compute_gaussnoise(_yrf, corr=noise[2], sigma=noise[3])
+yrf_err = SynthObs.compute_gaussnoise(_yrf, corr=noise_rf[0], sigma=noise_rf[1])
 yrf = _yrf + yrf_err
+noise = noise_swd + noise_rf
 
 
 #
@@ -65,19 +80,38 @@ dep, vs = np.loadtxt('observed/st3_mod.dat', usecols=[0, 2], skiprows=1).T
 pdep = np.concatenate((np.repeat(dep, 2)[1:], [150]))
 pvs = np.repeat(vs, 2)
 
-truenoise = np.concatenate(([noise[0]], [np.std(ysw_err)],   # target 1
-                            [noise[2]], [np.std(yrf_err)]))  # target 2
+selected_targets = []
+yobss = []
+ymods = []
+gauss = []
+truenoise = []
 
-explike = SynthObs.compute_explike(yobss=[ysw, yrf], ymods=[_ysw, _yrf],
-                                   noise=truenoise, gauss=[False, True],
-                                   rcond=initparams['rcond'])
+if args.inv_mode in ['joint', 'swd']:
+    selected_targets.append('swd')
+    yobss.append(ysw)
+    ymods.append(_ysw)
+    gauss.append(False)
+    truenoise.extend([noise_swd[0], np.std(ysw_err)])
+
+if args.inv_mode in ['joint', 'rf']:
+    selected_targets.append('rf')
+    yobss.append(yrf)
+    ymods.append(_yrf)
+    gauss.append(True)
+    truenoise.extend([noise_rf[0], np.std(yrf_err)])
+
+truenoise = np.asarray(truenoise)
+explike = SynthObs.compute_explike(
+    yobss=yobss, ymods=ymods, noise=truenoise, gauss=gauss,
+    rcond=initparams['rcond'])
 truemodel = {'model': (pdep, pvs),
              'nlays': 3,
              'noise': truenoise,
              'explike': explike,
              }
 
-print truenoise, explike
+print("Inversion mode: %s (%s)" % (args.inv_mode, ','.join(selected_targets)))
+print(truenoise, explike)
 
 
 #
@@ -88,13 +122,17 @@ print truenoise, explike
 # and phase velocity. Default is the fundamendal mode, but this can be updated.
 # For RF chose P or S. You can also use user defined targets or replace the
 # forward modeling plugin wih your own module.
-target1 = Targets.RayleighDispersionPhase(xsw, ysw, yerr=ysw_err)
-target2 = Targets.PReceiverFunction(xrf, yrf)
-target2.moddata.plugin.set_modelparams(gauss=1., water=0.01, p=6.4)
+targets_list = []
+if args.inv_mode in ['joint', 'swd']:
+    target_swd = Targets.RayleighDispersionPhase(xsw, ysw, yerr=ysw_err)
+    targets_list.append(target_swd)
+if args.inv_mode in ['joint', 'rf']:
+    target_rf = Targets.PReceiverFunction(xrf, yrf, yerr=yrf_err)
+    target_rf.moddata.plugin.set_modelparams(gauss=1., water=0.01, p=6.4)
+    targets_list.append(target_rf)
 
-# Join the targets. targets must be a list instance with all targets
-# you want to use for MCMC Bayesian inversion.
-targets = Targets.JointTarget(targets=[target1, target2])
+# Join the selected targets (one or multiple).
+targets = Targets.JointTarget(targets=targets_list)
 
 
 #
@@ -106,18 +144,24 @@ targets = Targets.JointTarget(targets=[target1, target2])
 # have station specific values, etc.
 # See docs/bayhunter.pdf for explanation of parameters
 
-priors.update({'mohoest': (38, 4),  # optional, moho estimate (mean, std)
-               'rfnoise_corr': 0.98,
-               'swdnoise_corr': 0.
-               # 'rfnoise_sigma': np.std(yrf_err),  # fixed to true value
-               # 'swdnoise_sigma': np.std(ysw_err),  # fixed to true value
-               })
+priors.update({'mohoest': (38, 4)})  # optional, moho estimate (mean, std)
+if args.inv_mode in ['joint', 'rf']:
+    priors.update({'rfnoise_corr': noise_rf[0]})
+if args.inv_mode in ['joint', 'swd']:
+    priors.update({'swdnoise_corr': noise_swd[0]})
 
 initparams.update({'nchains': 5,
                    'iter_burnin': (2048 * 32),
                    'iter_main': (2048 * 16),
                    'propdist': (0.025, 0.025, 0.015, 0.005, 0.005),
                    })
+
+# remove old chain files in the shared save directory to avoid
+# mixing different inversion modes (joint/rf/swd) in plotting/baywatch
+datadir = op.join(initparams['savepath'], 'data')
+for pattern in ['c???_p1*.npy', 'c???_p2*.npy', 'outliers.dat']:
+    for oldfile in glob.glob(op.join(datadir, pattern)):
+        os.remove(oldfile)
 
 
 #
